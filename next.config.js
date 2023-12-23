3b0f49fa43c55a54e8f9874f7ff8054aed6553ea
Sergio Campbell Dev <info.sergiocampbell@gmail.com>
2023-12-22 21:11:34 -0500

    chore: adjust cors config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
         source: "/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://podcaster-delta.vercel.app" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://podcaster-delta.vercel.app",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
